03e3f54a7980822cddbe107fc395702c7a91a712
nql1314 <494795532@qq.com>
2018-09-06 19:14:12 +0800

    update

--- JavaCore/src/main/java/Reflection/simpledemo/Service1.java
+++ JavaCore/src/main/java/Reflection/simpledemo/Service1.java
@@ -1,9 +1,7 @@
 package reflection.simpledemo;
 
-import Util.PrintUtil;
-
 public class Service1 {
     public void doService1(){
-        PrintUtil.print("service1");
+        System.out.print("service1");
     }
 }
--- JavaCore/src/main/java/Util/PrintUtil.java
+++
@@ -1,7 +0,0 @@
-package Util;
-
-public class PrintUtil {
-    public static void print(String str){
-        System.out.println(str);
-    }
-}
